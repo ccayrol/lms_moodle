906fd37607ccbba3b9efc675d5dfaa0fc0ec36eb
ccayrol <clement.cayrol@gmail.com>
2024-05-10 11:52:45 +0200

    logique algorithme

--- controle_coherence_fiche_liaison.py
+++ controle_coherence_fiche_liaison.py
@@ -1,6 +1,7 @@
 import csv
 import tkinter as tk
 from tkinter import filedialog
+import PyPDF2
 
 def controle_coherence(fichier_csv) :
     try : 
@@ -35,3 +36,60 @@ if __name__ == "__main__":
 
     # Lancer la boucle principale de l'interface graphique
     fenetre.mainloop()
+    
+    
+    
+# BASE D UTILISATION DE LIBRAIRIE PyPDF2 POUR ECRIRE DU TEXTE A DES COORDONNEES BIEN PRECISES.
+# OBJECTIF :
+#    - Pour chaque champs à remplir, identifier les coordonnees sur le pdf
+#    - Créer une liste qui se compose 
+#          . En 1er élément le texte à mettre dans le pdf
+#          . En 2eme une paire qui correspond aux coordonnées où il faut inserer le texte
+
+# EXEMPLE : liste_texte_coordonnees : 
+# liste_de_paires[] 
+# liste_de_paires.append(("test_Prenom", (10, 20)))
+# liste_de_paires.append(("test_Nom", (30, 40)))
+
+
+def write_on_existing_pdf(input_pdf, text, x, y, output_pdf):
+    
+    try : 
+        # Ouvre le fichier PDF existant en mode lecture binaire ('rb')
+        with open(input_pdf, 'rb') as file:
+            # Crée un objet PdfReader pour lire le fichier PDF
+            reader = PyPDF2.PdfReader(file)
+
+            # Crée un objet PdfWriter pour écrire dans le fichier PDF
+            writer = PyPDF2.PdfWriter()
+
+            # Parcours toutes les pages du fichier PDF
+            for page in reader.pages:
+                # Crée un objet PageObject pour la page actuelle
+                page_obj = writer.add_page(page)
+
+                # Crée un objet TextObject pour le texte à ajouter
+                text_obj = page_obj.getContents().getTextObject()
+
+                # Définit la position du texte
+                text_obj.setTextOrigin(x, y)
+
+                # Ajoute le texte à la position spécifiée
+                text_obj.textLine(text)
+
+            try :
+                # Écrit le contenu dans le fichier de sortie
+                with open(output_pdf, 'wb') as output_file:
+                    writer.write(output_file)
+            except FileNotFoundError:
+                print("Le fichier spécifié n'a pas été trouvé.")
+            except Exception as e:
+                print("Une erreur s'est produite :", e)    
+    except FileNotFoundError:
+        print("Le fichier spécifié n'a pas été trouvé.")
+    except Exception as e:
+        print("Une erreur s'est produite :", e)
+        
+
+# Appel de la fonction avec le nom du fichier PDF existant, le texte à écrire, les coordonnées x et y spécifiques, et le nom du fichier de sortie
+write_on_existing_pdf("input.pdf", "Ceci est du texte ajouté à des coordonnées précises.", 100, 750, "output.pdf")
