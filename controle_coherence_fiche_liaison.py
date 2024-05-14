d710dda90e60092375c8dc05fe5d2dda74250618
ccayrol <clement.cayrol@gmail.com>
2024-05-14 18:37:28 +0200

    fonctionnel

--- controle_coherence_fiche_liaison.py
+++ controle_coherence_fiche_liaison.py
@@ -23,16 +23,28 @@ import re
 import io
 
 
-def controle_coherence(fichier_csv) :
+def controle_coherence(fichier_csv, liste_coordonnees_texte) :
     try : 
+        cpt = 0
+        cpt_liste_coordonnees = 0 
         with open(fichier_csv,'r') as file :
-            reader = csv.reader(file, delimiter=';')
-            for row in reader :
-                print(row)
+            reader = csv.reader(file, delimiter=',')
+            for ligne in reader :
+                for element in ligne : 
+                    cpt += 1 
+                    print("cpt = "+ str(cpt)) 
+                    if cpt >= 33 and cpt_liste_coordonnees < len(liste_coordonnees_texte) :
+                        num_page, coordonnees, _ = liste_coordonnees_texte[cpt_liste_coordonnees]
+                        nouvel_element = (num_page,coordonnees,element)
+                        liste_coordonnees_texte[cpt_liste_coordonnees] = nouvel_element
+                        cpt_liste_coordonnees += 1
+                
     except FileNotFoundError:
         print("Le fichier spécifié n'a pas été trouvé.")
     except Exception as e:
         print("Une erreur s'est produite :", e)
+        
+    return liste_coordonnees_texte
         
 def choisir_fichier():
     # Ouvrir une boîte de dialogue pour choisir le fichier CSV
@@ -40,42 +52,23 @@ def choisir_fichier():
 
     # Si un fichier a été sélectionné, lire son contenu
     if chemin_fichier:
-        controle_coherence(chemin_fichier)
+        liste_de_paires = [] 
+        liste_de_paires.append((2, (51,63), "numetud"))
+        liste_de_paires.append((2, (36,69),"test_Nom"))
+        liste_de_paires.append((2, (112,69), "test_Prenom"))
+        liste_de_paires.append((2, (43,74), "Francais"))
+
+        print("ready to write on pdf\n")
+
+        write_data_to_pdf('C:\\workspace\\s10\\lms_moodle\\Fiche_de_liaison_Licence_2023-2024.pdf', "output.pdf", chemin_fichier, liste_de_paires)
+
+        print( "traitement terminé")
         
-      
         
-    
-    
-    
-# BASE D UTILISATION DE LIBRAIRIE PyPDF2 POUR ECRIRE DU TEXTE A DES COORDONNEES BIEN PRECISES.
-# OBJECTIF :
-#    - Pour chaque champs à remplir, identifier les coordonnees sur le pdf
-#    - Créer une liste qui se compose 
-#          . En 1er élément le texte à mettre dans le pdf
-#          . En 2eme une paire qui correspond aux coordonnées où il faut inserer le texte
-
-# EXEMPLE : liste_texte_coordonnees : 
-# liste_de_paires[] 
-# liste_de_paires.append(("test_Prenom", (10, 20)))
-# liste_de_paires.append(("test_Nom", (30, 40)))
-
-#Une fois que cette liste est complète, parcourir cette liste en appelant la fonction write_on_existing_pdf
-#exemple :
-# for texte_a_inserer, coordonnees in liste_de_paires :
-#    x, y = coordonnees
-#    write_on_existing_pdf(input_pdf, texte_a_inserer, x, y, output_pdf)
 
 
 
-
-
-#PERMET DE TROUVER LA POSITION DUN MOT DANS PDF --> FONCTIONNE
          
-def pixels_to_mm(pixels, dpi=72):
-    inches = pixels / dpi
-    mm = inches * 25.4  # 1 pouce = 25.4 millimètres
-    return mm
-
 def mm_to_points(mm):
     # Conversion de millimètres en points (1 mm = 2.83465 points)
     return mm * 2.83465
@@ -91,7 +84,8 @@ def find_keyword_position(pdf_path, keyword):
     return positions
 
 
-def write_data_to_pdf(input_pdf, output_pdf, liste_coordonnees_texte):
+def write_data_to_pdf(input_pdf, output_pdf, chemin_fichier, liste_coordonnees_texte):
+    liste_coordonnees_texte = controle_coherence(chemin_fichier, liste_coordonnees_texte)
     try :
         with open(input_pdf, 'rb') as file :
 
@@ -152,21 +146,4 @@ if __name__ == "__main__":
 
 
 # Implementer la liste de texte coordonnees, exemple ici    
-liste_de_paires = [] 
-liste_de_paires.append((2, (51,63), "numetud"))
-liste_de_paires.append((2, (36,69),"test_Nom"))
-liste_de_paires.append((2, (112,69), "test_Prenom"))
-liste_de_paires.append((2, (43,74), "Francais"))
 
-print("ready to write on pdf\n")
-
-
-positions_prenom = find_keyword_position('C:\\workspace\\s10\\lms_moodle\\Fiche_de_liaison_Licence_2023-2024.pdf', "Prénom")
-
-print("Positions de 'Prénom':", positions_prenom)
-
-
-
-write_data_to_pdf('C:\\workspace\\s10\\lms_moodle\\Fiche_de_liaison_Licence_2023-2024.pdf', "output.pdf", liste_de_paires)
-
-print( "traitement terminé")
