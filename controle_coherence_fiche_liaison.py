a77d1cd1e869780bae878981927d013f127c0dde
ccayrol <clement.cayrol@gmail.com>
2024-05-15 16:45:04 +0200

    ajout fonctionnalite fonction + modif csv de sortie

--- controle_coherence_fiche_liaison.py
+++ controle_coherence_fiche_liaison.py
@@ -22,7 +22,6 @@ def controle_coherence(fichier_csv, liste_coordonnees_texte) :
             for ligne in reader :
                 for element in ligne : 
                     cpt += 1 
-                    print("cpt = "+ str(cpt)) 
                     if cpt >= 33 and cpt_liste_coordonnees < len(liste_coordonnees_texte) :
                         num_page, coordonnees, _ = liste_coordonnees_texte[cpt_liste_coordonnees]
                         nouvel_element = (num_page,coordonnees,element)
@@ -46,16 +45,94 @@ def choisir_fichier():
         print("repertoire fichier = "+repertoire_fichier)
         output_fichier = repertoire_fichier +"\\output.pdf"
         liste_de_paires = [] 
-        liste_de_paires.append((2, (51,63), "numetud"))
-        liste_de_paires.append((2, (36,69),"test_Nom"))
-        liste_de_paires.append((2, (112,69), "test_Prenom"))
-        liste_de_paires.append((2, (43,74), "Francais"))
-        liste_de_paires.append((2, (54,85),""))   # NATIONNALITE
-        liste_de_paires.append((2, (54,85),""))   # ETAPE D ETUDE
-        liste_de_paires.append((2, (68,90),""))   # VOTRE UF 
-        liste_de_paires.append((2, (72,100),""))  # ADRESSE PERMARNENTE ETUDIANT
         
-
+        ########## PAGE 1 #######################################################
+        
+        liste_de_paires.append((2, (51,63), "numetud"))             #1
+        liste_de_paires.append((2, (36,69),"test_Nom"))             #2
+        liste_de_paires.append((2, (112,69), "test_Prenom"))        #3
+        liste_de_paires.append((2, (43,74), "Francais"))            #4
+        liste_de_paires.append((2, (54,84),""))     # ETAPE D ETUDE   5 
+        liste_de_paires.append((2, (68,89),""))     # VOTRE UF 6
+        liste_de_paires.append((2, (72,100),""))    # ADRESSE PERMARNENTE ETUDIANT 7
+        liste_de_paires.append((2, (45,105),""))    # CODE POSTAL 8
+        liste_de_paires.append((2, (42,110),""))    # COMMUNE 9
+        liste_de_paires.append((2, (36,115),""))    # PAYS 10
+        liste_de_paires.append((2, (42,121),""))    # TELEPHONE 11
+        liste_de_paires.append((2, (54,126),""))    # TELEPHONE PORTABLE 12
+        liste_de_paires.append((2, (72,131),""))    # COURRIEL ETUDIANT 13
+        liste_de_paires.append((2, (54,136),""))    # COURRIEL PERSONNEL 14
+        liste_de_paires.append((2, (57,165),""))    # DATE FIN DE VALIDITE 15
+        liste_de_paires.append((2, (60,170),""))    # COMPAGNIE ASSURANCE 16
+        liste_de_paires.append((2, (28,201),""))    # CAISSE PRIMIRE ASSURANCE MALADIE 17
+        liste_de_paires.append((2, (27,221),""))    # RAPPORT AVEC OFFRE DE STAGE DIFFUSE PAR UNIVERSITE = OUI 18
+        liste_de_paires.append((2, (42,221),""))    # RAPPORT AVEC OFFRE DE STAGE DIFFUSE PAR UNIVERSITE = NON 19
+        
+        
+        ############ PAGE 2 ################################################################################
+        
+        ############# ETABLISSEMENT ACCUEIL ##############################################
+        liste_de_paires.append((3, (73, 48),""))    # SIREN 20
+        liste_de_paires.append((3, (82, 53),""))    # RAISON SOCIALE ENTREPRISE 21
+        liste_de_paires.append((3, (68, 58),""))    # CODE APE 22
+        liste_de_paires.append((3, (103, 58),""))   # EFFECTIF 23
+        liste_de_paires.append((3, (63, 63),""))    # ACTIVITE DE L ETABLISSEMENT 24
+        liste_de_paires.append((3, (81, 68),""))    # TYPE D ETABLISSEMENT 25
+        liste_de_paires.append((3, (77, 72),""))    # ADRESSE ETABLISSEMENT 26
+        liste_de_paires.append((3, (47, 77),""))    # CODE POSTAL 27
+        liste_de_paires.append((3, (95, 77),""))    # COMMUNE 28
+        liste_de_paires.append((3, (38, 82),""))    # PAYS 29 
+        liste_de_paires.append((3, (73, 87),""))    # NUMERO TEL ETABLISSEMENT 30
+        liste_de_paires.append((3, (71, 92),""))    # SERVICE ET LIEUX PRECIS STAGE 31
+        
+        
+        ############## TUTEUR DE STAGE DANS ORGANISME ACCUEIL ###########################3
+        
+        liste_de_paires.append((3, (27, 107),""))    # CASE "MONSIEUR" 32
+        liste_de_paires.append((3, (38.3, 107),""))    # CASE "MADAME" 33
+        liste_de_paires.append((3, (71, 107),""))    # NOM PRENOM 34
+        liste_de_paires.append((3, (51, 113),""))    # TELEPHONE 35
+        liste_de_paires.append((3, (90, 113),""))    # MEL 36
+        liste_de_paires.append((3, (44, 120),""))    # FONCTION 37
+    
+        ############# CONTENU DU STAGE ###################################################
+        
+        liste_de_paires.append((3, (47.5, 135),""))    # STAGE OBLIGATOIRE 38
+        liste_de_paires.append((3, (72.5, 135),""))    # STAGE OPTIONNEL 39
+        liste_de_paires.append((3, (96.5, 135),""))    # STAGE EN FRANCE 40
+        liste_de_paires.append((3, (120.8, 135),""))    # STAGE A L ETRANGER 41
+        
+        liste_de_paires.append((3, (61, 141),""))    # CODE UE 42
+        liste_de_paires.append((3, (125, 141),""))    # NOMBRE ECTS 43
+        liste_de_paires.append((3, (58, 148),""))    # THEMATIQUE DU STAGE 44
+        # SI THEMATIQUE TROP LONG CHANGER DE LIGNE ET PASSER A (30,152)
+        liste_de_paires.append((3, (113, 154.5),""))    # SUJET DE STAGE  45
+        # SI SUJET TROP LONG CHANGER DE LIGNE ET PASSER A (30,159)
+        liste_de_paires.append((3, (133, 160.5),""))    # FONCTION ET TACHES CONFIEES AU STAGIAIRE 46
+        # SI TROP LONG PASSER A (30,165), PUIS (30,168)
+        liste_de_paires.append((3, (103, 170),""))  # COMETENCES A ACQUERIR PENDANT STAGE 47
+        # SI TROP LONG, (30,174) PUIS (30, 178)
+        
+        
+        
+        ############ DATE HORAIRES DEROULEMENT DU STAGE ###################################
+        
+        liste_de_paires.append((3, (53, 190.5),""))    # DEBUT DE STAGE 48
+        liste_de_paires.append((3, (50, 194),""))    # FIN DU STAGE 49
+        liste_de_paires.append((3, (67.3, 197.5),""))    # INTERUPTION EN COURS DU STAGE OUI 50
+        liste_de_paires.append((3, (74.7, 197.5),""))    # INTERUPTION EN COURS DU STAGE NON 51
+        liste_de_paires.append((3, (77, 201),""))    # DATE DEBUT INTERRUPTION 52
+        liste_de_paires.append((3, (74, 204),""))    # DATE FIN INTERRUPTION 53
+        liste_de_paires.append((3, (112, 211),""))    # DUREE EFFECTIF DU STAGES EN HEURES 54
+        liste_de_paires.append((3, (85.5, 224),""))    # NOMBRE DE JOUR DE TRAVAIL HEBDO 55
+        liste_de_paires.append((3, (50.5, 228.1),""))    # TEMPS DE TRAVAIL = TEMPS PLEINS 56
+        liste_de_paires.append((3, (69.3, 228.1),""))    # TEMPS DE TRAVAIL = TEMPS PARTIEL 57
+        liste_de_paires.append((3, (73, 234),""))    # NOMBRE D HEURE HEBDOMADAIRE 58
+        liste_de_paires.append((3, (78, 244),""))    # COMMENTAIRE SUR LE TEMPS DE TRAVAIL 59
+        # SI TROP LONG, (30,248)
+        liste_de_paires.append((3, (78, 257),""))    # NOMBRE JOUR DE CONGES AUTORISES 60
+        
+        
         print("ready to write on pdf\n")
 
         write_data_to_pdf('C:\\workspace\\s10\\lms_moodle\\Fiche_de_liaison_Licence_2023-2024.pdf', output_fichier, chemin_fichier, liste_de_paires)
@@ -89,7 +166,8 @@ def write_data_to_pdf(input_pdf, output_pdf, chemin_fichier, liste_coordonnees_t
                     if page_numero == page_num :    
                         packet = io.BytesIO()
                         can = canvas.Canvas(packet, pagesize=letter)
-                        can.drawString(mm_to_points(x)+10, mm_to_points(297) - mm_to_points(y) - 5, texte_a_inserer)  # Ajouter à une position spécifique
+                        can.setFont('Helvetica', 10)
+                        can.drawString(mm_to_points(x)+ 5, mm_to_points(297) - mm_to_points(y) - 5, texte_a_inserer)  # Ajouter à une position spécifique
                         can.save()
                         packet.seek(0)
                         new_pdf = PyPDF2.PdfReader(packet)
